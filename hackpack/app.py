55ade0394d9f6aeec45eb9808a0c009b449bed0a
PandaPony <ok@pandapony.com>
2017-03-15 13:47:01 +0100

    Update app.py

--- hackpack/app.py
+++ hackpack/app.py
@@ -30,7 +30,7 @@ def voice():
     if found_e164_pstn:
         to = "{0}".format(found_e164_pstn.group(1))
     elif found_0041_pstn:
-        to = "+{0}".format(found_011_pstn.group(1))
+        to = "+41{0}".format(found_011_pstn.group(1))
     elif found_ch_pstn:
         to = "+1{0}".format(found_us_pstn.group(1))
 
